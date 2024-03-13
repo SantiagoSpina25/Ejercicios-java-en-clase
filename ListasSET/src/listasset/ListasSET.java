fe0d0e64d39b9f812a66cf65fc61066430be286d
Medac <Medac@DESKTOP-3DEPRML>
2024-03-13 14:43:42 +0100

    13-03-24

---
+++ ListasSET/src/listasset/ListasSET.java
@@ -0,0 +1,84 @@
+
+package listasset;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+public class ListasSET {
+
+    
+    public static void main(String[] args) {
+        HashSet<Integer> numeros = new HashSet<>();
+        //Tambien se puede hacer asi:    Set numeros = new HashSet<Integer>();
+        
+        numeros.add(5);
+        numeros.add(3);
+        numeros.add(2);
+        numeros.add(9);
+                
+        System.out.println(numeros);
+        numeros.add(9);
+        System.out.println(numeros);
+        
+        
+        for (int i = 1; i < 11; i++) {
+            if(numeros.contains(i)){
+                System.out.println("La lista contiene el numero: " + i);
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        HashSet<String> nombres = new HashSet<>();
+         nombres.add("Juan");
+         nombres.add("Pedro");
+         nombres.add("Zorra");
+         nombres.add("Abeja");
+         nombres.add("Vicente");
+         
+//         System.out.println(nombres);
+        
+         Iterator<String> it = nombres.iterator();
+         //Para iterar
+//         while(it.hasNext()){
+//             System.out.println("Nombre es: " + it.next());
+//         }
+         String prueba = "Pedro";
+         
+         //Comprobar si contiene
+//         System.out.println(nombres.contains(prueba));
+         
+         
+        HashSet<Persona> personas = new HashSet<>();
+//        personas.add(new Persona("Alex", 0, "111"));
+//        personas.add(new Persona("Pepe", 20, "777"));
+//        
+//        
+//        Persona p1 = new Persona("Elena", 30, "77");
+//        personas.add(p1);
+//        System.out.println(personas);
+//        personas.add(p1);
+//        System.out.println(personas);
+//        personas.contains(p1);
+//        
+//        System.out.println(personas.contains(p1));
+//        
+//        Persona p2 = new Persona("Elena", 30, "77");
+//        personas.add(p2);
+//        System.out.println(personas);
+//        
+        Persona p1 = new Persona("Juan", 77, "777");
+        Persona p2 = new Persona("Juan", 77, "777");
+        personas.add(p1);
+        personas.add(p2);
+        System.out.println(personas);
+    }
+    
+}
+ 
---
+++ ListasSET/src/listasset/Persona.java
@@ -0,0 +1,57 @@
+
+package listasset;
+
+import java.util.Objects;
+
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String dni;
+
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", edad=" + edad + ", dni=" + dni + '}';
+    }
+
+    @Override
+    public int hashCode() {
+//        int hash = 5;
+//        hash = 97 * hash + Objects.hashCode(this.nombre);
+//        hash = 97 * hash + this.edad;
+//        hash = 97 * hash + Objects.hashCode(this.dni);
+//        return hash;
+        int a = (int) (Math.random()*10);
+        return a;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (this.edad != other.edad) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        return Objects.equals(this.dni, other.dni);
+    }
+
+
+    
+}
